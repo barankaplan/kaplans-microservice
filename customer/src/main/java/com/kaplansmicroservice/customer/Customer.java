229c10822ec09af5ec00371a8d7fdbc9cfabf088
Baran Kaplan <info@barankaplan.com>
2022-07-03 08:05:47 +0300

    jars-are-ready

--- customer/src/main/java/com/kaplansmicroservice/customer/Customer.java
+++ customer/src/main/java/com/kaplansmicroservice/customer/Customer.java
@@ -5,17 +5,14 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
-import javax.persistence.SequenceGenerator;
-import javax.persistence.GenerationType;
+import javax.persistence.*;
 
 @Data
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor
 @Entity
+@Table(name = "customer")
 public class Customer {
 
     @Id
